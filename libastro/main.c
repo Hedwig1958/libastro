8613b1d3c85fb384add7fa1b114c49d630774406
Corto Maltese <hedwig.delaere@skynet.be>
2015-02-05 17:55:48 +0100

    Initial Commit

---
+++ libastro/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libastro
+//
+//  Created by Corto Maltese on 5/02/15.
+//  Copyright (c) 2015 Corto Maltese. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
